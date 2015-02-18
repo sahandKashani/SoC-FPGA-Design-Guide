fb96a22a0832093926f7c7e3507cbd6e8f99c332
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-02-18 14:46:00 +0100

    Added header to generate_hps_qsys_header.sh for file tracking.

--- Distribution/Scripts/generate_hps_qsys_header.sh
+++ Distribution/Scripts/generate_hps_qsys_header.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+################################################################################
+# generate_hps_qsys.ds
+#
+# Author        : Sahand Kashani-Akhavan from Altera documentation
+# Revision      : 1.1
+# Creation date : 18/02/2015
+###############################################################################
+
 sopc-create-header-files \
 "RTL/quartus/soc_system.sopcinfo" \
 --single "hps_soc_system.h" \
