5cd178ccc7b3ba2d95f4e4902825cfeefdcacd85
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-02-19 14:27:26 +0100

    Remove redundant call to function in hps_baremetal.h

--- DE1_SoC_demo/C/ds5/projects/DE1_SoC_demo_hps_baremetal/hps_baremetal.h
+++ DE1_SoC_demo/C/ds5/projects/DE1_SoC_demo_hps_baremetal/hps_baremetal.h
@@ -12,9 +12,9 @@
 // |   HPS_LED   |  GPIO53  |   GPIO1[24]  |    I/O   |
 // |=============|==========|==============|==========|
 #define HPS_LED_IDX      (ALT_GPIO_1BIT_53)
-#define HPS_LED_MASK     (1 << alt_gpio_bit_to_port_pin(HPS_LED_IDX))
 #define HPS_LED_PORT     (alt_gpio_bit_to_pid(HPS_LED_IDX))
 #define HPS_LED_PORT_BIT (alt_gpio_bit_to_port_pin(HPS_LED_IDX))
+#define HPS_LED_MASK     (1 << HPS_LED_PORT_BIT)
 
 // |=============|==========|==============|==========|
 // | Signal Name | HPS GPIO | Register/bit | Function |
@@ -22,9 +22,9 @@
 // |   HPS_KEY   |  GPIO54  |   GPIO1[25]  |    I/O   |
 // |=============|==========|==============|==========|
 #define HPS_KEY_IDX      (ALT_GPIO_1BIT_54)
-#define HPS_KEY_MASK     (1 << alt_gpio_bit_to_port_pin(HPS_KEY_IDX))
 #define HPS_KEY_PORT     (alt_gpio_bit_to_pid(HPS_KEY_IDX))
 #define HPS_KEY_PORT_BIT (alt_gpio_bit_to_port_pin(HPS_KEY_IDX))
+#define HPS_KEY_MASK     (1 << HPS_KEY_PORT_BIT)
 
 // The 7-segment display is active low
 #define HEX_DISPLAY_CLEAR (0x7F)
