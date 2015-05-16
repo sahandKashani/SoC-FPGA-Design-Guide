21a7324a682893476e618a59c704ee67ef51fe3f
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-16 12:26:09 +0200

    Automatically create destination folder in generate_hps_qsys_header.sh if it does not exist

--- Distribution/Scripts/generate_hps_qsys_header.sh
+++ Distribution/Scripts/generate_hps_qsys_header.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ################################################################################
-# generate_hps_qsys.ds
+# generate_hps_qsys_header.sh
 #
 # Author        : Sahand Kashani-Akhavan from Altera documentation
-# Revision      : 1.2
+# Revision      : 1.3
 # Creation date : 18/02/2015
 ###############################################################################
 
@@ -16,5 +16,9 @@ sopc-create-header-files \
 "RTL/quartus/soc_system.sopcinfo" \
 --single "hps_soc_system.h" \
 --module "hps_0"
+
+if [ ! -d "C/ds5/projects/" ]; then
+    mkdir "C/ds5/projects/"
+fi
 
 mv "hps_soc_system.h" "C/ds5/projects/"
