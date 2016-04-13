328fccc624dbf96510c25712d3327c2ead90d4bf
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2016-04-13 17:13:35 +0200

    Re-indent top-level

--- DE1_SoC_demo/hw/hdl/DE1_SoC_top_level.vhd
+++ DE1_SoC_demo/hw/hdl/DE1_SoC_top_level.vhd
@@ -3,27 +3,27 @@ use ieee.std_logic_1164.all;
 
 entity DE1_SoC_top_level is
     port(
-        -- -- ADC
-        -- ADC_CS_n         : out   std_logic;
-        -- ADC_DIN          : out   std_logic;
-        -- ADC_DOUT         : in    std_logic;
-        -- ADC_SCLK         : out   std_logic;
+     -- -- ADC
+     -- ADC_CS_n         : out   std_logic;
+     -- ADC_DIN          : out   std_logic;
+     -- ADC_DOUT         : in    std_logic;
+     -- ADC_SCLK         : out   std_logic;
 
-        -- -- Audio
-        -- AUD_ADCDAT       : in    std_logic;
-        -- AUD_ADCLRCK      : inout std_logic;
-        -- AUD_BCLK         : inout std_logic;
-        -- AUD_DACDAT       : out   std_logic;
-        -- AUD_DACLRCK      : inout std_logic;
-        -- AUD_XCK          : out   std_logic;
+     -- -- Audio
+     -- AUD_ADCDAT       : in    std_logic;
+     -- AUD_ADCLRCK      : inout std_logic;
+     -- AUD_BCLK         : inout std_logic;
+     -- AUD_DACDAT       : out   std_logic;
+     -- AUD_DACLRCK      : inout std_logic;
+     -- AUD_XCK          : out   std_logic;
 
-        -- -- CLOCK
+     -- -- CLOCK
         CLOCK_50         : in    std_logic;
-        -- CLOCK2_50        : in    std_logic;
-        -- CLOCK3_50        : in    std_logic;
-        -- CLOCK4_50        : in    std_logic;
+     -- CLOCK2_50        : in    std_logic;
+     -- CLOCK3_50        : in    std_logic;
+     -- CLOCK4_50        : in    std_logic;
 
-        -- SDRAM
+     -- -- SDRAM
         DRAM_ADDR        : out   std_logic_vector(12 downto 0);
         DRAM_BA          : out   std_logic_vector(1 downto 0);
         DRAM_CAS_N       : out   std_logic;
@@ -36,11 +36,11 @@ entity DE1_SoC_top_level is
         DRAM_UDQM        : out   std_logic;
         DRAM_WE_N        : out   std_logic;
 
-        -- -- I2C for Audio and Video-In
-        -- FPGA_I2C_SCLK    : out   std_logic;
-        -- FPGA_I2C_SDAT    : inout std_logic;
+     -- -- I2C for Audio and Video-In
+     -- FPGA_I2C_SCLK    : out   std_logic;
+     -- FPGA_I2C_SDAT    : inout std_logic;
 
-        -- SEG7
+     -- -- SEG7
         HEX0_N           : out   std_logic_vector(6 downto 0);
         HEX1_N           : out   std_logic_vector(6 downto 0);
         HEX2_N           : out   std_logic_vector(6 downto 0);
@@ -48,50 +48,50 @@ entity DE1_SoC_top_level is
         HEX4_N           : out   std_logic_vector(6 downto 0);
         HEX5_N           : out   std_logic_vector(6 downto 0);
 
-        -- -- IR
-        -- IRDA_RXD         : in    std_logic;
-        -- IRDA_TXD         : out   std_logic;
+     -- -- IR
+     -- IRDA_RXD         : in    std_logic;
+     -- IRDA_TXD         : out   std_logic;
 
-        -- KEY_n
+     -- -- KEY_N
         KEY_N            : in    std_logic_vector(3 downto 0);
 
-        -- LED
+     -- -- LED
         LEDR             : out   std_logic_vector(9 downto 0);
 
-        -- -- PS2
-        -- PS2_CLK          : inout std_logic;
-        -- PS2_CLK2         : inout std_logic;
-        -- PS2_DAT          : inout std_logic;
-        -- PS2_DAT2         : inout std_logic;
+     -- -- PS2
+     -- PS2_CLK          : inout std_logic;
+     -- PS2_CLK2         : inout std_logic;
+     -- PS2_DAT          : inout std_logic;
+     -- PS2_DAT2         : inout std_logic;
 
-        -- SW
+     -- -- SW
         SW               : in    std_logic_vector(9 downto 0);
 
-        -- -- Video-In
-        -- TD_CLK27         : inout std_logic;
-        -- TD_DATA          : out   std_logic_vector(7 downto 0);
-        -- TD_HS            : out   std_logic;
-        -- TD_RESET_N       : out   std_logic;
-        -- TD_VS            : out   std_logic;
+     -- -- Video-In
+     -- TD_CLK27         : inout std_logic;
+     -- TD_DATA          : out   std_logic_vector(7 downto 0);
+     -- TD_HS            : out   std_logic;
+     -- TD_RESET_N       : out   std_logic;
+     -- TD_VS            : out   std_logic;
 
-        -- -- VGA
-        -- VGA_B            : out   std_logic_vector(7 downto 0);
-        -- VGA_BLANK_N      : out   std_logic;
-        -- VGA_CLK          : out   std_logic;
-        -- VGA_G            : out   std_logic_vector(7 downto 0);
-        -- VGA_HS           : out   std_logic;
-        -- VGA_R            : out   std_logic_vector(7 downto 0);
-        -- VGA_SYNC_N       : out   std_logic;
-        -- VGA_VS           : out   std_logic;
+     -- -- VGA
+     -- VGA_B            : out   std_logic_vector(7 downto 0);
+     -- VGA_BLANK_N      : out   std_logic;
+     -- VGA_CLK          : out   std_logic;
+     -- VGA_G            : out   std_logic_vector(7 downto 0);
+     -- VGA_HS           : out   std_logic;
+     -- VGA_R            : out   std_logic_vector(7 downto 0);
+     -- VGA_SYNC_N       : out   std_logic;
+     -- VGA_VS           : out   std_logic;
 
-        -- -- GPIO_0
-        -- GPIO_0           : inout std_logic_vector(35 downto 0);
+     -- -- GPIO_0
+     -- GPIO_0           : inout std_logic_vector(35 downto 0);
 
-        -- -- GPIO_1
-        -- GPIO_1           : inout std_logic_vector(35 downto 0);
+     -- -- GPIO_1
+     -- GPIO_1           : inout std_logic_vector(35 downto 0);
 
-        -- -- HPS
-        -- HPS_CONV_USB_N   : inout std_logic;
+     -- -- HPS
+     -- HPS_CONV_USB_N   : inout std_logic;
         HPS_DDR3_ADDR    : out   std_logic_vector(14 downto 0);
         HPS_DDR3_BA      : out   std_logic_vector(2 downto 0);
         HPS_DDR3_CAS_N   : out   std_logic;
@@ -117,38 +117,38 @@ entity DE1_SoC_top_level is
         HPS_ENET_RX_DV   : in    std_logic;
         HPS_ENET_TX_DATA : out   std_logic_vector(3 downto 0);
         HPS_ENET_TX_EN   : out   std_logic;
-        -- HPS_FLASH_DATA   : inout std_logic_vector(3 downto 0);
-        -- HPS_FLASH_DCLK   : out   std_logic;
-        -- HPS_FLASH_NCSO   : out   std_logic;
-        -- HPS_GPIO         : inout std_logic_vector(1 downto 0);
-        -- HPS_GSENSOR_INT  : inout std_logic;
-        -- HPS_I2C_CONTROL  : inout std_logic;
-        -- HPS_I2C1_SCLK    : inout std_logic;
-        -- HPS_I2C1_SDAT    : inout std_logic;
-        -- HPS_I2C2_SCLK    : inout std_logic;
-        -- HPS_I2C2_SDAT    : inout std_logic;
-        HPS_KEY_N          : inout std_logic;
+     -- HPS_FLASH_DATA   : inout std_logic_vector(3 downto 0);
+     -- HPS_FLASH_DCLK   : out   std_logic;
+     -- HPS_FLASH_NCSO   : out   std_logic;
+     -- HPS_GPIO         : inout std_logic_vector(1 downto 0);
+     -- HPS_GSENSOR_INT  : inout std_logic;
+     -- HPS_I2C_CONTROL  : inout std_logic;
+     -- HPS_I2C1_SCLK    : inout std_logic;
+     -- HPS_I2C1_SDAT    : inout std_logic;
+     -- HPS_I2C2_SCLK    : inout std_logic;
+     -- HPS_I2C2_SDAT    : inout std_logic;
+        HPS_KEY_N        : inout std_logic;
         HPS_LED          : inout std_logic;
         HPS_SD_CLK       : out   std_logic;
         HPS_SD_CMD       : inout std_logic;
         HPS_SD_DATA      : inout std_logic_vector(3 downto 0);
-        -- HPS_SPIM_CLK     : out   std_logic;
-        -- HPS_SPIM_MISO    : in    std_logic;
-        -- HPS_SPIM_MOSI    : out   std_logic;
-        -- HPS_SPIM_SS      : inout std_logic;
+     -- HPS_SPIM_CLK     : out   std_logic;
+     -- HPS_SPIM_MISO    : in    std_logic;
+     -- HPS_SPIM_MOSI    : out   std_logic;
+     -- HPS_SPIM_SS      : inout std_logic;
         HPS_UART_RX      : in    std_logic;
         HPS_UART_TX      : out   std_logic
-        -- HPS_USB_CLKOUT   : in    std_logic;
-        -- HPS_USB_DATA     : inout std_logic_vector(7 downto 0);
-        -- HPS_USB_DIR      : in    std_logic;
-        -- HPS_USB_NXT      : in    std_logic;
-        -- HPS_USB_STP      : out   std_logic
+     -- HPS_USB_CLKOUT   : in    std_logic;
+     -- HPS_USB_DATA     : inout std_logic_vector(7 downto 0);
+     -- HPS_USB_DIR      : in    std_logic;
+     -- HPS_USB_NXT      : in    std_logic;
+     -- HPS_USB_STP      : out   std_logic
     );
 end entity DE1_SoC_top_level;
 
 architecture rtl of DE1_SoC_top_level is
     component soc_system is
-        port (
+        port(
             clk_clk                               : in    std_logic                     := 'X';
             reset_reset_n                         : in    std_logic                     := 'X';
             buttons_0_external_connection_export  : in    std_logic_vector(3 downto 0)  := (others => 'X');
@@ -216,7 +216,7 @@ architecture rtl of DE1_SoC_top_level is
 
 begin
     soc_system_inst : component soc_system
-        port map (
+        port map(
             clk_clk                               => CLOCK_50,
             reset_reset_n                         => '1',
             buttons_0_external_connection_export  => KEY_N,
@@ -235,8 +235,8 @@ begin
             sdram_controller_0_wire_cke           => DRAM_CKE,
             sdram_controller_0_wire_cs_n          => DRAM_CS_N,
             sdram_controller_0_wire_dq            => DRAM_DQ,
-            sdram_controller_0_wire_dqm(1)           => DRAM_UDQM,
-            sdram_controller_0_wire_dqm(0)           => DRAM_LDQM,
+            sdram_controller_0_wire_dqm(1)        => DRAM_UDQM,
+            sdram_controller_0_wire_dqm(0)        => DRAM_LDQM,
             sdram_controller_0_wire_ras_n         => DRAM_RAS_N,
             sdram_controller_0_wire_we_n          => DRAM_WE_N,
             hps_0_ddr_mem_a                       => HPS_DDR3_ADDR,
@@ -281,5 +281,4 @@ begin
             hps_0_io_hps_io_gpio_inst_GPIO53      => HPS_LED,
             hps_0_io_hps_io_gpio_inst_GPIO54      => HPS_KEY_N
         );
-
 end;
